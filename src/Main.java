a2cb28a6a62405e42a8e53ca11393929bb2803f7
Ezra Micah Malsi <66657798+ZaZra03@users.noreply.github.com>
2022-09-04 12:20:57 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,6 @@
 import java.io.*;
 
 public class Main {
-
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		int n, lowestNum, secondLowestNum;	
