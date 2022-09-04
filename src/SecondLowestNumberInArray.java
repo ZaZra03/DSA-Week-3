883ec17454d73fc8bebba72aa5dbc96c11c3ee62
Ezra Micah Malsi <malsiezramicah3@gmail.com>
2022-09-04 13:02:37 +0800

    Update file

--- src/SecondLowestNumberInArray.java
+++ src/SecondLowestNumberInArray.java
@@ -42,18 +42,20 @@ public class SecondLowestNumberInArray {
 			//Asks the user's prefer size that will be allocated in array
 			System.out.println("How many numbers?");
 			n = Integer.parseInt(in.readLine());
+			
 			if(n != 2) {
-				int[] numArr = new int[n];
-				
 				//Gets the user's input that will be stored in array
+				int[] numArr = new int[n];
 				for(int i = 0; i < n; i++) {
 					System.out.print("Enter a number: ");
 					numArr[i] = Integer.parseInt(in.readLine());
 				}
 				secondlargestNumber(numArr, n);
-			} else System.out.println("Invalit input.");
-
-		} catch (Exception e){
+			} 
+			else System.out.println("Invalit input.");
+		} 
+		
+		catch (Exception e){
 			System.out.println("Please enter a number.");
 			return;
 		}
