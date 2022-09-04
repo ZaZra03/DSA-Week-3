90c5f9f2f0b66efa6a8a93cf91e873ca4b823e27
Ezra Micah Malsi <malsiezramicah3@gmail.com>
2022-09-04 12:20:05 +0800

    Add comments

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,19 @@ public class Main {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		int n, lowestNum, secondLowestNum;	
 		
+		//Asks the user's prefer size that will be allocated in array
 		System.out.println("How many numbers?");
 		n = Integer.parseInt(in.readLine());
 		
 		int[] numArr = new int[n];
 		
+		//Gets the user's input that will be stored in array
 		for(int i = 0; i < n; i++) {
 			System.out.print("Enter a number: ");
 			numArr[i] = Integer.parseInt(in.readLine());
 		}
 		
+		//Get the lowest and second lowest number
 		if(numArr[0] < numArr[1]) {
 			lowestNum = numArr[0];
 			secondLowestNum = numArr[1];
@@ -26,6 +29,11 @@ public class Main {
 			secondLowestNum = numArr[0];
 		}
 		
+		/*
+		 * Check the other elements stored in array and compare it to the lowest number.
+		 * The value of lowest and second lowest number might change if the condition
+		 * has met
+		 */
 		for(int i = 2; i < numArr.length; i++) {
 			if(numArr[i] < lowestNum) {
 				secondLowestNum = lowestNum;
