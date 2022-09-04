54c1037aa37c07c489919f9b959304ec2de769fb
Ezra Micah Malsi <malsiezramicah3@gmail.com>
2022-09-04 12:33:04 +0800

    Renamed class name

--- src/SecondLowestNumberInArray.java
+++ src/SecondLowestNumberInArray.java
@@ -1,6 +1,6 @@
 import java.io.*;
 
-public class SecondLargestNumberInArray {
+public class SecondLowestNumberInArray {
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		int n, lowestNum, secondLowestNum;	
